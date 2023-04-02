6a2f1cea426212af6261fb1f1f14615f4d2731ca
nbhaohao <nbhaohao@163.com>
2023-04-02 19:35:14 +0800

    Big-Endian & Little-Endian

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <sybdb.h>
 
-int main() {
-    printf("Hello, World\n");
-    return 0;
+int a = 10;
+
+BOOL IsBigEndian() {
+  int a = 0x1234;
+  char b = *(char *) &a; //通过将int强制类型转换成char单字节，通过判断起始存储位置。即等于 取b等于a的低地址部分
+  if (b == 0x12) {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+int main(int argc, char *argv[]) {
+  printf("%d", IsBigEndian());
+  char ch1;
+  printf("please input a number\r\n");
+  scanf("%d", &ch1);
+  printf("sizeof%d\r\n", sizeof(ch1));
+  printf("%c\r\n", ch1);
+  return 0;
 }
